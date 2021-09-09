061126021e959732289403c693e5b9611fb63df9
trje9656 mac <trje9656@colorado.edu>
2021-09-08 18:35:12 -0600

    Initial Commit

---
+++ Lab1_DancingPics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1_DancingPics
+//
+//  Created by Trevor Jedziniak on 9/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
